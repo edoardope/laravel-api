61c935f83ba35250ce6029253d5292da27d39bec
edoardope <pencoedoardo@gmail.com>
2023-07-06 15:56:55 +0200

    chiamata api con whit

--- app/Http/Controllers/Api/ApiProjectController.php
+++ app/Http/Controllers/Api/ApiProjectController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Project;
+use App\Models\Admin\Technology;
+use App\Models\Admin\Type;
 use Illuminate\Http\Request;
 
 class ApiProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('type', 'technologies')->get();
 
         return response()->json([
             'succes' => true,
